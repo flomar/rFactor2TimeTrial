29149f69a42100ee9dee7ebe0462dceb42c70d91
flomar <florian@marchal.de>
2016-07-04 11:06:43 +0200

    restructured files, and refactored INSTALL document

--- trunk/Installer/createReleaseZip.pl
+++ trunk/Installer/createReleaseZip.pl
@@ -10,11 +10,8 @@
 # This script assumes both sub projects, QTimeTrial and rF2TimeTrial, have
 # been built successfully before being invoked. Depending on your local
 # environment, a few tweaks might be necessary for the script to run
-# properly.
-#
-# Any questions? Drop me an email at florian(at)marchal(dot)de.
-#
-# flomar, July 2016
+# properly, especially with regards to external commands such as "rm"
+# or "7z".
 #
 
 use POSIX;
@@ -27,7 +24,7 @@ sleep(1);
 my $releaseName = "rFactor2TimeTrial" . "-" . strftime('%Y%m%d%H%M%S',localtime);
 
 # assemble path to the release folder
-my $releaseFolder = "../releases/$releaseName";
+my $releaseFolder = "../../releases/$releaseName";
 
 # create the release folder structure
 mkdir("$releaseFolder");
@@ -37,13 +34,14 @@ mkdir("$releaseFolder/rF2TimeTrial");
 mkdir("$releaseFolder/rF2TimeTrial/win32");
 mkdir("$releaseFolder/rF2TimeTrial/win64");
 
-# copy LICENSE and INSTALL files
-copy("../LICENSE", "$releaseFolder");
+# copy LICENSE, and INSTALLand README files
+copy("../../LICENSE", "$releaseFolder");
 copy("INSTALL", "$releaseFolder");
+copy("README", "$releaseFolder");
 
 # copy QTimeTrial files
 my @dllFiles = ();
-find(\&findDllFile, "QTimeTrial/bin");
+find(\&findDllFile, "../QTimeTrial/bin");
 sub findDllFile {
     my $dllFile = $_;
     if($dllFile =~ m/.+\.dll/) {
@@ -51,19 +49,19 @@ sub findDllFile {
     }
 }
 foreach my $dllFile (@dllFiles) {
-    copy("QTimeTrial/bin/$dllFile", "$releaseFolder/QTimeTrial/win32");
+    copy("../QTimeTrial/bin/$dllFile", "$releaseFolder/QTimeTrial/win32");
 }
-copy("QTimeTrial/build/release/release/QTimeTrial.exe", "$releaseFolder/QTimeTrial/win32");
-copy("QTimeTrial/QTimeTrial.sqlite", "$releaseFolder/QTimeTrial");
+copy("../QTimeTrial/build/release/release/QTimeTrial.exe", "$releaseFolder/QTimeTrial/win32");
+copy("../QTimeTrial/QTimeTrial.sqlite", "$releaseFolder/QTimeTrial");
 copy("QTimeTrial/QTimeTrial.bat", "$releaseFolder/QTimeTrial");
 
 # copy rF2TimeTrial files
-copy("rF2TimeTrial/Win32/Release/rF2TimeTrial.dll", "$releaseFolder/rF2TimeTrial/win32");
-copy("rF2TimeTrial/x64/Release/rF2TimeTrial.dll", "$releaseFolder/rF2TimeTrial/win64");
+copy("../rF2TimeTrial/Win32/Release/rF2TimeTrial.dll", "$releaseFolder/rF2TimeTrial/win32");
+copy("../rF2TimeTrial/x64/Release/rF2TimeTrial.dll", "$releaseFolder/rF2TimeTrial/win64");
 copy("rF2TimeTrial/rF2TimeTrial.cfg", "$releaseFolder/rF2TimeTrial");
 
 # compress the whole thing into a convenient ZIP archive
-system("/c/Program\\ Files/7-Zip/7z.exe a ../releases/$releaseName.zip $releaseFolder");
+system("/c/Program\\ Files/7-Zip/7z.exe a ../../releases/$releaseName.zip $releaseFolder");
 
 # remove the temporary release folder
 system("rm -rf $releaseFolder");
